aebc52d1f69526e391c15bc8ac233e4a26dcc4ff
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2024-11-26 01:13:02 +0530

    feat: added initial route and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,30 @@ import { RouterOutlet } from '@angular/router';
 import { ApiService } from './api-service';
 import { HttpClientModule } from '@angular/common/http';
 import { FileUploadComponent } from './upload-file/file-upload.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, FileUploadComponent],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    HeaderComponent,
+    FileUploadComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'full_stack_practice';
-  message = ''
+  message = '';
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getData().subscribe((data) => {
       console.log(data);
-      this.message = data ? data['message']: ''
+      this.message = data ? data['message'] : '';
     });
   }
 }
